c7e75fd3f95297e376685aa2ddbe88b04f0dee76
Madhavivanesh R <madhavivanesh@gmail.com>
2026-04-06 15:49:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Show, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, Button } from '@chakra-ui/react';
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GameHeading from "./components/GameHeading";
@@ -19,7 +19,7 @@ export interface GameQuery {
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
-  
+   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Grid
       templateAreas={{
@@ -41,6 +41,29 @@ function App() {
       </Show>
       <GridItem area="main">
         <Box paddingLeft={2}>
+          {/* MOBILE GENRE DRAWER */}
+        <Show below="lg">
+          <Button onClick={onOpen} marginBottom={3} colorScheme="blue">
+            Select Genre
+          </Button>
+          <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Genres</DrawerHeader>
+              <DrawerBody>
+                <GenreList 
+                  selectedGenre={gameQuery.genre} 
+                  onSelectGenre={(genre) => {
+                    setGameQuery({ ...gameQuery, genre });
+                    onClose(); // Automatically closes menu when they pick one
+                  }} 
+                />
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </Show>
+        {/* END OF MOBILE GENRE DRAWER */}
           <GameHeading gameQuery={gameQuery} />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
